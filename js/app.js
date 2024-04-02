f6a00c800093bfa98e53eb65c2683d7128e4792d
Dejan <dkilaz@gmail.com>
2024-04-02 10:31:42 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,71 @@
+class CalorieTracker {
+  constructor() {
+    this._calorieLimit = 2200;
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+
+    this._displayCaloriesTotal();
+    this._displayCaloriesLimit();
+  }
+
+  // Public Methods/API //
+
+  addMeal(meal) {
+    this._meals.push(meal);
+    this._totalCalories += meal.calories;
+    this._render();
+  }
+
+  addWorkout(workout) {
+    this._meals.push(workout);
+    this._totalCalories -= workout.calories;
+    this._render();
+  }
+
+  // Private Methods //
+
+  _displayCaloriesTotal() {
+    const totalCaloriesEl = document.getElementById('calories-total');
+    totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById('calories-limit');
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
+  }
+}
+
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
+tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
+
+const run = new Workout('Morning run', 300);
+tracker.addWorkout(run);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
